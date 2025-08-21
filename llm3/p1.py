37cfbd5416d7b87bb9bc7cbaa7922a2413c54120
jeanm <jmlee@tonesol.com>
2025-08-21 15:00:02 +0900

    0821

--- llm3/p1.py
+++ llm3/p1.py
@@ -13,14 +13,14 @@ text = st.text_area(label="질문입력:",
                     placeholder="질문을 입력 하세요")
 if st.button("SEND"):
     if text:
-        # Progress Bar
+        # Progress Bar Start -----------------------------------------
         progress_text = "Operation in progress. Please wait."
         my_bar = st.progress(0, text=progress_text)
         for percent_complete in range(100):
             time.sleep(0.08)
             my_bar.progress(percent_complete + 1, text=progress_text)
         time.sleep(1)
-        # Progress Bar
+        # Progress Bar End -----------------------------------------
 
         result = geminiTxt(text)
         my_bar.empty()
