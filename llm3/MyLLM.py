de4381051ce0bcfaa4648075ccd67c8c332680a0
jeanm <jmlee@tonesol.com>
2025-08-21 17:03:43 +0900

    0821

--- llm3/MyLLM.py
+++ llm3/MyLLM.py
@@ -1,11 +1,9 @@
-from openai import OpenAI
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY")
 def geminiModel():
     genai.configure(api_key=GOOGLE_API_KEY)
