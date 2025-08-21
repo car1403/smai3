1bc32954f3a30a9cba7c403d270b1ad5d27e7d43
jeanm <jmlee@tonesol.com>
2025-08-21 10:44:03 +0900

    0821

--- llm2/open2.py
+++ llm2/open2.py
@@ -3,10 +3,10 @@ from myllm.MyApi import openAiModelArg, makeMsg
 
 def test(prompt):
     modelName = "gpt-4o"
-    msg = makeMsg("너는 친절한 한국어 선생님",prompt)
+    msg = makeMsg("한문장으로 요약해줘",prompt)
     result = openAiModelArg(modelName, msg)
     print(result)
 
 if __name__ == '__main__':
-    prompt = "우간다의 수도가 어디야"
+    prompt = "SK하이닉스에 대해서 알려줘"
     test(prompt)
