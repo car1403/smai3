28034f2d1eef25f1cf5b06c10f7c8aa48cffb7b6
jeanm <jmlee@tonesol.com>
2025-08-21 15:23:10 +0900

    0821

--- llm3/p2.py
+++ llm3/p2.py
@@ -8,16 +8,9 @@ def save_uploadedfile(directory, file):
     # 1. 디렉토리가 없으면 생성
     if not os.path.exists(directory):
         os.makedirs(directory)
-
     # 2. 파일 저장 (이름 변경 없이 저장)
-    st.info(file.name.split('.'))
-
     with open(os.path.join(directory, file.name), 'wb') as f:
         f.write(file.getbuffer())
-    print(os.path.join(directory, file.name))
-    print(file.name.split('.')[0])
-    Image.open(os.path.join(directory, file.name)).save(os.path.join(directory, file.name.split('.')[0] + ".png"))
-
     # 3. 저장 완료 메시지 출력
     st.success(f'저장 완료: {directory}에 {file.name} 저장되었습니다.')
 
