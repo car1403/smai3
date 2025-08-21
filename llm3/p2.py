70cbd71afe6ec6e03f43e9f1dd82d10f863a9e80
jeanm <jmlee@tonesol.com>
2025-08-21 15:19:15 +0900

    0821

--- llm3/p2.py
+++ llm3/p2.py
@@ -1,10 +1,37 @@
+import os
+
 import streamlit as st
+from PIL import Image
+
+
+def save_uploadedfile(directory, file):
+    # 1. 디렉토리가 없으면 생성
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # 2. 파일 저장 (이름 변경 없이 저장)
+    st.info(file.name.split('.'))
+
+    with open(os.path.join(directory, file.name), 'wb') as f:
+        f.write(file.getbuffer())
+    print(os.path.join(directory, file.name))
+    print(file.name.split('.')[0])
+    Image.open(os.path.join(directory, file.name)).save(os.path.join(directory, file.name.split('.')[0] + ".png"))
+
+    # 3. 저장 완료 메시지 출력
+    st.success(f'저장 완료: {directory}에 {file.name} 저장되었습니다.')
+
 
 # Sidebar
 st.sidebar.markdown("Clicked Page 2")
 
 # Page
-st.title("Page 2")
+st.title("Page 2 Image Upload")
+file = st.file_uploader('이미지 파일 업로드', type=['png', 'jpg', 'jpeg', 'webp'])
+if file:
+    st.image(file)
+    save_uploadedfile("img", file)
+
 
 
 
