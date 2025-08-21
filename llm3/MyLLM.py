ddb2d23e3225d423319824e65bf4830d66d649ac
jeanm <jmlee@tonesol.com>
2025-08-21 17:15:46 +0900

    0821

--- llm3/MyLLM.py
+++ llm3/MyLLM.py
@@ -1,10 +1,14 @@
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
+import streamlit as st
 
 load_dotenv()
 
-GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY")
+#GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY")
+GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
+
+
 def geminiModel():
     genai.configure(api_key=GOOGLE_API_KEY)
     model = genai.GenerativeModel("gemini-2.0-flash")
