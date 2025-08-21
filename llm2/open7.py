bb78f37c5dd7cbc290b5274ed11bf75e13804351
jeanm <jmlee@tonesol.com>
2025-08-21 13:28:25 +0900

    0821

---
+++ llm2/open7.py
@@ -0,0 +1,18 @@
+from myllm.MyApi import openAiModelArg, makeMsg, openAiModel
+
+def test(prompt):
+    openModel = openAiModel()
+    response = openModel.images.create_variation(
+        model="dall-e-2",
+        image=open(prompt, "rb"),
+        n=3,
+        size="1024x1024"
+    )
+    for n,data in enumerate(response.data):
+        print(n)
+        print(data.url)
+
+
+if __name__ == '__main__':
+    prompt = "img/dogncat.png"
+    test(prompt)
