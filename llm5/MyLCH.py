30d8d73f57d31aaa3bd3f5e0352d907023975e6a
jeanm <jmlee@tonesol.com>
2025-08-25 15:35:37 +0900

    0825

--- llm5/MyLCH.py
+++ llm5/MyLCH.py
@@ -1,9 +1,12 @@
+import time
+
 from langchain_community.chat_models import ChatOpenAI
 from langchain_google_genai import ChatGoogleGenerativeAI
 from openai import OpenAI
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
+import streamlit as st
 
 load_dotenv()
 
@@ -24,3 +27,31 @@ def getGenAI():
         google_api_key=GOOGLE_API_KEY
     )
     return llm
+
+def progressBar(txt):
+    # Progress Bar Start -----------------------------------------
+    progress_text = txt
+    my_bar = st.progress(0, text=progress_text)
+    for percent_complete in range(100):
+        time.sleep(0.08)
+        my_bar.progress(percent_complete + 1, text=progress_text)
+    time.sleep(1)
+    return my_bar
+    # Progress Bar End -----------------------------------------
+
+def openAiModel():
+    client = OpenAI(api_key=OPENAI_API_KEY)
+    return client
+def makeAudio(text, name):
+    if not os.path.exists("audio"):
+        os.makedirs("audio")
+    model = openAiModel()
+    response = model.audio.speech.create(
+        model="tts-1",
+        input=text,
+        #["alloy", "echo", "fable", "onyx", "nova", "shimmer"],
+        voice="echo",
+        response_format="mp3",
+        speed=1.2,
+    )
+    response.stream_to_file("audio/"+name)
